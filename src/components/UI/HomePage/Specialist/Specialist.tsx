0cea3bc25241715e949599462e8fdab0ead3734a
rakib-molla <rakibmail1997@gmail.com>
2024-03-19 22:25:36 +0600

    display sepcialiest data and apply style

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Container, Typography } from "@mui/material"
+import { Box, Button, Container, Typography,Stack } from "@mui/material"
+import Image from "next/image";
 
 const Specialist = async() =>{
     const res = await fetch('http://localhost:5000/api/v1/specialties',{
@@ -6,7 +7,7 @@ const Specialist = async() =>{
             revalidate: 30,
         }
     });
-    const specialties = await res.json();
+    const {data:specialties} = await res.json();
     console.log(specialties);
 
   return (
@@ -17,6 +18,37 @@ const Specialist = async() =>{
         <Box>
             <Typography variant="h4" fontWeight={600}>Explore Treatments Across Specialties</Typography>
             <Typography component="p" fontWeight={300} fontSize={18}>Experienced Doctors Across All Specialties</Typography>
+            <Stack direction="row" gap={4} mt={5}>
+                {specialties.map((specialty:any)=>(
+                    <Box key={specialty.id} sx={{
+                        flex:1,
+                        width:"150px",
+                        backgroundColor: "rgba(245,245,245,1)",
+                        border:"1px solid rgba(250,250,250,1)",
+                        borderRadius:"10px",
+                        textAlign:"center",
+                        padding:"40px 10px",
+                        "& img":{
+                            width:"50px",
+                            height:"50px",
+                            margin:"0 auto"
+                        },
+                        "&:hover":{
+                            border:"1px solid blue",
+                            cursor:"pointer",
+                        }
+                    }}>
+                        <Image src={specialty.icon} width={100} height={100} alt="specialty icon"/>
+                        <Typography component="p" fontWeight={600} fontSize={18} mt={2}>{specialty.title}</Typography>
+
+                    </Box>
+                ))}
+            </Stack>
+            <Box display="flex" alignItems="center" justifyContent="center">
+                <Button variant="outlined" sx={{
+                marginTop: "20px",
+            }}>View All</Button>
+            </Box>
         </Box>
     </Container>
   )
