56f9cc2cb1130b375b71df0d16de112978acb5b7
rakib-molla <rakibmail1997@gmail.com>
2024-03-19 21:38:46 +0600

    fetch specialities data and create sepecilities section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection"
+import Specialist from "@/components/UI/HomePage/Specialist/Specialist"
 import { Button } from "@mui/material"
 
 
@@ -6,6 +7,7 @@ function HomePage() {
   return (
     <>
       <HeroSection/>
+      <Specialist/>
     </>
   )
 }
--- src/components/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/components/UI/HomePage/HeroSection/HeroSection.tsx
@@ -18,7 +18,6 @@ function HeroSection() {
                 width: "700px",
                 top: "-90px",
                 left: "-120px",
-                
             }}>
                 <Image src={assets.svgs.grid} alt="grid"/>
             </Box>
@@ -26,7 +25,7 @@ function HeroSection() {
             <Typography variant="h3" component="h1" fontWeight={600}>Comes From</Typography>
             <Typography color="primary.main" variant="h3" component="h1" fontWeight={600}>Preventive Care</Typography>
             <Typography color="#808080" variant="h6" component="p" fontWeight={400} sx={{
-                width:"50%",
+                
                 py: "25px",
             }}>
                 development is being shaped by technological developments, artificial intelligence (AI), and the growth of no-code/low-code platforms. AI is not expected to replace frontend developers
@@ -36,7 +35,45 @@ function HeroSection() {
                 marginLeft: "10px"
             }} variant="outlined">Contact Us </Button>
         </Box>
-        <Box>Right</Box>
+
+        <Box sx={{
+            p:1,
+            flex: 1,
+            display: "flex",
+            justifyContent:"center",
+            position: "relative",
+            mt: 0,
+        }}>
+            <Box sx={{
+                position: "absolute",
+               left: "200px",
+               top: "-30px",
+            }}>
+                <Image src={assets.svgs.arrow} width={100} height={100} alt="arrow"/>
+            </Box>
+            <Box sx={{
+                display: "flex",
+                gap: 2,
+            }}>
+                <Box
+                mt={4}
+                ><Image src={assets.images.doctor1} width={240} height={380} alt="doctor1"/></Box>
+                <Box><Image src={assets.images.doctor2} width={240} height={350} alt="doctor2"/></Box>
+            </Box>
+            <Box sx={{
+                position: "absolute",
+                 top:"220px",
+                 left: "150px",
+                }}><Image src={assets.images.doctor3} width={240} height={240} alt="doctor3"/>
+            </Box>
+            <Box sx={{
+                position: "absolute",
+                 bottom:"-50px",
+                 right:"0",
+                 zIndex: "-1"
+                }}><Image src={assets.images.stethoscope} width={180} height={180} alt="doctor3"/>
+            </Box>
+        </Box>
     </Container>
   )
 }
