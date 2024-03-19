6211659aaff1f5d6229c1af617476b2fb8260695
rakib-molla <rakibmail1997@gmail.com>
2024-03-20 00:06:33 +0600

    create top rated doctors section with data fetching

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection"
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist"
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors"
 import { Button } from "@mui/material"
 
 
@@ -8,6 +9,7 @@ function HomePage() {
     <>
       <HeroSection/>
       <Specialist/>
+      <TopRatedDoctors/>
     </>
   )
 }
