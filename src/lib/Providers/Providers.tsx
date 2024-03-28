a02f24d10ba2feeec5e5c0a15c25ecde25c17b69
rakib-molla <rakibmail1997@gmail.com>
2024-03-28 17:00:10 +0600

    redux and axios setup in this project

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -3,10 +3,14 @@
 
 import { ThemeProvider } from "@mui/material"
 import { theme } from "../theme/theme"
+import { Provider } from "react-redux"
+import { store } from "@/redux/store"
 
 function Providers({children}: {children: React.ReactNode}) {
   return (
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <Provider store={store}> 
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
   )
 }
 
